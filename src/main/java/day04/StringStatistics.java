fb4f2ffaa6813b9e6f5f589ae9319b983a84850a
Bruzsa László <lacika007@gmail.com>
2022-01-20 16:21:07 +0100

    day04

--- src/main/java/day04/StringStatistics.java
+++ src/main/java/day04/StringStatistics.java
@@ -8,7 +8,7 @@ import static java.util.stream.Collectors.groupingBy;
 public class StringStatistics {
 
     public Map<Character, Long> getVowelsCount(String text) {
-        return text.chars()
+        return text.toLowerCase().chars()
                 .mapToObj(value -> (char) value)
                 .filter(character -> switch (character) {
                     case 'a', 'e', 'i', 'o', 'u' -> true;
